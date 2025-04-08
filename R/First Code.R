7c15d7ae5bbf83126bd691947842d79df629ffbb
bilol20 <banerjeebilol@gmail.com>
2025-04-08 13:16:14 +0530

    Updates

--- R/First Code.R
+++ R/First Code.R
@@ -7,7 +7,7 @@
 # @param X, Y, Z sampled data
 # @param R number of iterations of the resampling algorithm
 
-Test.CTS = function(X,Y,Z, R){
+Test.CTS = function(X,Y,Z, R = 200){
   if(class(X)[1] == "numeric"){
     n = length(X)
   }else{
@@ -32,4 +32,32 @@ Test.CTS = function(X,Y,Z, R){
 
   pval = (1+sum(S1>S)+sum(S1==S))/(1+R)
   return(pval)
+}
+
+BD.ctest = function(X,Y,Z, R = 200){
+  if(class(X)[1] == "numeric"){
+    n = length(X)
+  }else{
+    n = nrow(X)
+  }
+  X1 = cbind(X,Z)
+  Y1 = cbind(Y,Z)
+
+  V = rbind(X1,Y1)
+  m = 2*n
+  D = as.matrix(dist(V))
+  S = bd(D, distance = TRUE, size = c(n,n))
+
+  v = rbinom(R*n,1,0.5)
+  S1 = lapply(1:R,function(l){
+    index = which(v[1:n + l - 1]==1)
+    pi = 1:(2*n)
+    pi[c(index,n+index)] = pi[c(n+index,index)]
+    D1 = D[pi,pi]
+    return(bd(D1, distance = TRUE, size = c(n,n)))
+  })
+
+  pval = (1+sum(S1>S)+sum(S1==S))/(1+R)
+  return(pval)
+
 }
